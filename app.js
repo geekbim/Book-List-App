b2f3ce022a983378b9a8a566e77b46fd25fe7b12
geekbim <manyuabim9@gmail.com>
2021-02-28 22:19:11 +0700

    add route

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const connectDB = require('./config/db')
+var cors = require('cors')
+
+// routes
+const books = require('./routes/api/books')
 
 const app = express()
 
 // connect db
 connectDB()
 
+// cors
+app.use(cors({ origin: true, credentials: true }));
+
+// init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+// use routes
+app.use('/api/books', books)
+
 
 const port = process.env.PORT || 8082
 
